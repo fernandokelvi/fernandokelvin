0729de25b3bd36451371b71906e5ed2cd1411a00
Fernando kelvin <fernandokelb582@gmail.com>
2022-05-16 11:23:00 -0300

    Add files via upload

---
+++ cronologia-marvel-2021-a-2023/README.md
@@ -0,0 +1,4 @@
+# Cronologia Marvel 2021 a 2023
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Melvink/pen/BaYBPgo](https://codepen.io/Melvink/pen/BaYBPgo).
+
---
+++ cronologia-marvel-2021-a-2023/dist/script.js
@@ -0,0 +1,15 @@
+var listaFilmes = [
+  "https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg",
+
+  "https://media.fstatic.com/7tLJ6lI8r_u4EDPPLhiz9RGNDig=/290x478/smart/media/movies/covers/2022/03/halfstack_busshelter_48x70_moonknight_v7_lg_0.jpg",
+  "https://terrigen-cdn-dev.marvel.com/content/prod/1x/doctorstrangeinthemultiverseofmadness_teaser2_printed_1-sht_v4_lg.jpg",
+  "https://disneyplusbrasil.com.br/wp-content/uploads/2021/10/image-124.png",
+  "https://upload.wikimedia.org/wikipedia/pt/0/08/Thor_Love_and_Thunder_poster.jpg",
+  "https://assets.mycast.io/posters/black-panther-wakanda-forever-fan-casting-poster-151282-large.jpg?1638989355",
+  "https://br.web.img3.acsta.net/pictures/21/07/12/17/51/1270341.jpg",
+  ""
+];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
